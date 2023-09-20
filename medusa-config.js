e241b43673639830b736c1c0e55a0428d25855b5
musarri <msaricicek_99@hotmail.com>
2023-09-20 18:13:56 +0300

    module export changed

--- medusa-config.js
+++ medusa-config.js
@@ -82,7 +82,10 @@ const projectConfig = {
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
-  projectConfig,
+  projectConfig: {
+    // ...
+    database_extra: { ssl: { rejectUnauthorized: false } },
+  },
   plugins,
   modules,
 };
